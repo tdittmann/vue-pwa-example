109a1a7812b2178442aa22494fa4232e7be10c7c
Timo Dittmann <timo.dittmann@mail.schwarz>
2025-07-01 15:42:27 +0200

    Add VitePWA

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,33 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      filename: 'service-worker.js',
+      manifest: {
+        name: "Vue PWA Example",
+        short_name: "VPE",
+        icons: [
+          {src: '/img/icons/icon-48x48.png', sizes: '48x48', type: 'image/png'},
+          {src: '/img/icons/icon-72x72.png', sizes: '72x72', type: 'image/png'},
+          {src: '/img/icons/icon-96x96.png', sizes: '96x96', type: 'image/png'},
+          {src: '/img/icons/icon-128x128.png', sizes: '128x128', type: 'image/png'},
+          {src: '/img/icons/icon-144x144.png', sizes: '144x144', type: 'image/png'},
+          {src: '/img/icons/icon-152x152.png', sizes: '152x152', type: 'image/png'},
+          {src: '/img/icons/icon-192x192.png', sizes: '192x192', type: 'image/png'},
+          {src: '/img/icons/icon-256x256.png', sizes: '256x256', type: 'image/png'},
+          {src: '/img/icons/icon-384x384.png', sizes: '384x384', type: 'image/png'},
+          {src: '/img/icons/icon-512x512.png', sizes: '512x512', type: 'image/png'},
+        ]
+      }
+    })
   ],
   resolve: {
     alias: {
